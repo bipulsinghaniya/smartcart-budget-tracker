0d1d1cbcc70345981b27acd9641af0e8c5bc05b5
Bipul Singhaniya <singhaniyabipul9@gmail.com>
2025-12-26 23:09:32 +0530

    i have added

--- my-react-app/backend/server.js
+++ my-react-app/backend/server.js
@@ -1,18 +1,14 @@
 const express = require("express");
-const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-
+const cors = require("cors");
 dotenv.config();  //  done 
-connectDB();   // done 
-
 const app = express();  // done 
-
 app.use(cors());  // done 
 app.use(express.json()); // done 
 
+connectDB();   // done 
 app.use("/api/auth", require("./routes/auth"));
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
